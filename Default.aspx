6de7c59e8bc10cbe2ae66dd6822b92bc44e0c4d7
Tom Navarro <tom@ansync.com>
2013-06-23 17:03:51 -0700

    Moving from Visual Studio project folder to git

---
+++ Default.aspx
@@ -0,0 +1,30 @@
+﻿<%@ Page Title="Home Page" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeFile="Default.aspx.cs" Inherits="_Default" %>
+
+<asp:Content runat="server" ID="FeaturedContent" ContentPlaceHolderID="FeaturedContent">
+    <section class="featured">
+        <div class="content-wrapper">
+            <hgroup class="title">
+                <h1><%: Title %>.</h1>
+                <h2>Modify this template to jump-start your ASP.NET application.</h2>
+            </hgroup>
+            <p>
+                To learn more about ASP.NET, visit <a href="http://asp.net" title="ASP.NET Website">http://asp.net</a>.
+                The page features <mark>videos, tutorials, and samples</mark> to help you get the most from
+                ASP.NET. If you have any questions about ASP.NET visit
+                <a href="http://forums.asp.net/18.aspx" title="ASP.NET Forum">our forums</a>.
+            </p>
+        </div>
+    </section>
+</asp:Content>
+
+<asp:Content runat="server" ID="BodyContent" ContentPlaceHolderID="MainContent">
+    <label for="typesDropDown">Document Type</label>
+    <asp:DropDownList ID="typesDropDown" runat="server"></asp:DropDownList>
+    <label for="titleSearchBox">Document Title</label>
+    <asp:TextBox ID="titleSearchBox" runat="server"></asp:TextBox>
+    <asp:Button ID="searchButton" runat="server" Text="Search" onClick="doSearch"/>
+
+    <asp:GridView ID="documentGrid" runat="server" AutoGenerateColumns="True" AllowPaging="True" AllowSorting="True">
+    </asp:GridView>
+    <asp:SqlDataSource runat="server" ID="MmsyDataSource" ConnectionString='<%$ ConnectionStrings:MmsydataConnectionString %>' ProviderName='<%$ ConnectionStrings:MmsydataConnectionString.ProviderName %>' SelectCommand="SELECT [objtype] FROM [Type]"></asp:SqlDataSource>
+</asp:Content>
---
+++ Default.aspx.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Configuration;
+using System.Data.OleDb;
+
+
+public partial class _Default : Page {
+    protected void Page_Load(object sender, EventArgs e) {
+        if (!Page.IsPostBack) {
+            ListItem hide = new ListItem("------------------------", "NULL");
+            hide.Attributes.Add("disabled", "disabled");
+            typesDropDown.AppendDataBoundItems = true;
+            typesDropDown.Items.Insert(0, new ListItem("Any Type", "NULL"));
+            typesDropDown.Items.Insert(1, hide);
+            typesDropDown.DataSourceID = "MmsyDataSource";
+            typesDropDown.DataTextField = "objtype";
+            typesDropDown.DataValueField = "objtype";
+        }
+    }
+    protected void doSearch(object sender, EventArgs e) {
+        /* Search query:
+         *     SELECT MIMSY1.Sorts, MIMSY1.objname AS [Title], MIMSY1.description AS [Description], MIMSY1.objtype AS [Object Type], MIMSY1.Key1
+         *     FROM   (DOCUMENTS INNER JOIN MIMSY1 ON DOCUMENTS.[counter] = MIMSY1.[counter]), Type
+         *     << If there's a value in the drop-down, concat WHERE  (MIMSY1.objtype = <<drop down value as string>>) >>
+         *     << If there's a value in the text box,
+         *         << If there's a value in the drop-down, concat AND >>
+         *         concat WHERE (MIMSY1.objname LIKE '*' + text.value + '*')
+         *         If title searching, try to sort exact matches at the top...sort decending by relevance
+         */
+        bool addAnd = false;
+        string query = "SELECT MIMSY1.Sorts, MIMSY1.objname AS [Title], MIMSY1.description AS [Description], MIMSY1.objtype AS [Object Type], MIMSY1.Key1 FROM (DOCUMENTS INNER JOIN MIMSY1 ON DOCUMENTS.[counter] = MIMSY1.[counter]), Type";
+        OleDbConnection dConnection = new OleDbConnection(ConfigurationManager.ConnectionStrings["MmsydataConnectionString"].ConnectionString);
+        OleDbCommand dCommand;
+        OleDbDataAdapter dAdapter;
+        System.Data.DataSet ds = new System.Data.DataSet();
+
+        if (typesDropDown.SelectedIndex != 0) {
+            query += " WHERE (MIMSY1.objtype = '" + typesDropDown.SelectedItem.Value + "')";
+            addAnd = true;
+        }
+
+        if (titleSearchBox.Text.Length > 0) {
+            if (addAnd) {
+                query += " AND ";
+            }
+            else {
+                query += " WHERE ";
+            }
+
+            query += "(MIMSY1.objname LIKE '%" + titleSearchBox.Text + "%')";
+        }
+
+        dCommand = new OleDbCommand(query, dConnection);
+        dConnection.Open();
+        dAdapter = new OleDbDataAdapter(dCommand);
+        dAdapter.Fill(ds);
+        dConnection.Close();
+        documentGrid.DataSource = ds;
+        documentGrid.DataBind();
+    }
+}
